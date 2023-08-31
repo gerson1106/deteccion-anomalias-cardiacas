6d92ca8588ef4ad9899a8139f4282e747e7f96dd
gerson1106 <ghernandez6@udi.edu.co>
2023-08-31 15:26:59 -0500

    Se agrega el proyecto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Despliegue en Streamlit del proyecto "Detección de anomalías cardiacas usando autoencoders"
+
+Código para realizar el despliegue en *Streamlit Cloud* del modelo explicado en el [Despliegue de un modelo en la nube con Streamlit Cloud](https://www.codificandobits.com/blog/tutorial-despliegue-en-la-nube-streamlit-cloud/).
---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from utils import *
+autoencoder = Autoencoder()
+autoencoder = torch.load('C:/Users/gerso/OneDrive/Escritorio/ls/prod/autoencoder.pth')
+autoencoder.eval()
+#autoencoder = cargar_modelo_preentrenado(r"/autoencoder.pth")
+# Características básicas de la página
+st.set_page_config(page_icon="🌳", page_title="Detección de vegetación Santurban", layout="wide")
+st.image("https://web.udi.edu.co/aviacion/wp-content/uploads/2020/09/sRecurso-1.png", width=200)
+st.title("Detección de vegetación Paramo de Santurban")
+
+c29, c30, c31 = st.columns([1, 9, 1]) # 3 columnas: 10%, 60%, 10%
+
+UMBRAL = 0.089
+
+with c30:
+    uploaded_file = st.file_uploader(
+        "", type = 'pkl',
+        key="1",
+    )
+
+
+    if uploaded_file is not None:
+        file_container = st.expander("Verifique el archivo .pkl que acaba de subir")
+
+        info_box_wait = st.info(
+            f"""
+                Realizando la clasificación...
+                """)
+
+        # Acá viene la predicción con el modelo
+        dato = leer_dato(uploaded_file)
+        #autoencoder = Autoencoder()
+        #autoencoder = cargar_modelo_preentrenado('autoencoder.pth')
+        prediccion = predecir(autoencoder, dato, UMBRAL)
+        categoria = obtener_categoria(prediccion)
+
+
+        # Y mostrar el resultado
+        info_box_result = st.info(f"""
+        	El dato analizado corresponde a un sujeto: {categoria}
+        	""")
+
+    else:
+        st.info(
+            f"""
+                👆 Debe cargar primero un dato con extensión .pkl
+                """
+        )
+
+        st.stop()
+        
+
